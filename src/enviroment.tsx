fa7354bfa73a6406ba10a355c957ce3bcaaf7299
Vincent <vincent1906@live.nl>
2019-09-10 13:10:14 +0200

    Finished feature
    Als een gebruiker de website opent krijgt hij/zij een pagina om in te loggen

---
+++ src/enviroment.tsx
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    environmentName: 'LOCAL',
+    API_KEY: 'YOUR_API_KEY',
+    API_URL: `http://localhost:3000`,
+    LOGIN_URL: 'http://localhost:3000/login',
+
+};
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,26 +1,95 @@
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonInput, IonButton, IonList, IonItem, IonLabel } from '@ionic/react';
+import React, { FormEvent, useState } from 'react';
+import axios from 'axios';
+import { environment } from '../enviroment';
+import { jsxElement } from '@babel/types';
+import { encode } from 'punycode';
 
-const Home: React.FunctionComponent = () => {
-  return (
-    <>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Ionic Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent className="ion-padding">
-        The world is your oyster.
-        <p>
-          If you get lost, the{' '}
-          <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/">
-            docs
-          </a>{' '}
-          will be your guide.
-        </p>
-      </IonContent>
-    </>
-  );
-};
+type MyProps = {};
+type MyState = { email: string, password: string };
 
-export default Home;
+
+export default class HomePage extends React.Component<MyProps, MyState> {
+
+  config = {
+    headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/x-www-form-urlencoded' }
+  };
+
+  constructor(props: any) {
+    super(props);
+    console.log('constructing!');
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
+
+  async handleSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    try {
+
+      const user = await this.login(this.state.email, this.state.password);
+
+      // ...
+    } catch (e) {
+      console.error(e);
+    }
+  }
+  handleEmailChange(event: any) {
+    this.setState({ 'email': event.target.value });
+  }
+  handlePasswordChange(event: any) {
+    this.setState({ 'password': event.target.value });
+  }
+
+  login(email: string, password: string) {
+
+    const object: any = { 'email': email, 'password': password };
+
+
+    var formBody = [];
+    for (var property in object) {
+      var encodedKey = encodeURIComponent(property);
+      var encodedValue = encodeURIComponent(object[property]);
+      formBody.push(encodedKey + "=" + encodedValue);
+    }
+    const formBodyString = formBody.join("&");
+
+
+    console.log(environment.LOGIN_URL);
+    axios.post(environment.LOGIN_URL, formBodyString, this.config).then(response => response.data)
+      .then((data) => {
+        console.log(data)
+      })
+  }
+
+
+
+  render() {
+    return (
+      <>
+        <IonHeader>
+          <IonToolbar color="primary">
+            <IonTitle>Login</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent>
+          <form onSubmit={e => this.handleSubmit(e)} action="post">
+            <IonList>
+              <IonItem>
+                <IonLabel>Email</IonLabel>
+                <IonInput name="email" type="email" value={this.state.email} onInput={(e: any) => this.handleEmailChange(e)} />
+              </IonItem>
+              <IonItem>
+                <IonLabel>Password</IonLabel>
+                <IonInput name="password" type="password" value={this.state.password} onInput={(e: any) => this.handlePasswordChange(e)} />
+              </IonItem>
+              <IonButton type="submit">Log in</IonButton>
+            </IonList>
+          </form>
+        </IonContent>
+      </>
+    );
+  }
+}
