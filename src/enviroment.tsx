05194f290aae3c7c6214a39dc4d89d4ad1fa0bce
Vincent <vincent1906@live.nl>
2019-11-05 14:10:55 +0100

    Changed enviroment to match server ip

--- src/enviroment.tsx
+++ src/enviroment.tsx
@@ -2,7 +2,7 @@ export const environment = {
     production: false,
     environmentName: 'LOCAL',
     API_KEY: 'YOUR_API_KEY',
-    API_URL: `http://127.0.0.1:8000/api/`,
-    LOGIN_URL: 'http://127.0.0.1:8000/api/login',
-    APPOINTMENT_URL: 'http://127.0.0.1:8000/api/appointment/',
+    API_URL: `http://77.162.207.35/api/`,
+    LOGIN_URL: 'http://77.162.207.35/api/login',
+    APPOINTMENT_URL: 'http://77.162.207.35/api/appointment/',
 };
