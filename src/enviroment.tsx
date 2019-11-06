a803ee13a192be41fc00f9748fa3a86c4b975bc1
Vincent <vincent1906@live.nl>
2019-11-06 10:14:48 +0100

    Removed https

--- src/enviroment.tsx
+++ src/enviroment.tsx
@@ -2,7 +2,7 @@ export const environment = {
     production: false,
     environmentName: 'LOCAL',
     API_KEY: 'YOUR_API_KEY',
-    API_URL: `https://77.162.207.35/api/`,
-    LOGIN_URL: 'https://77.162.207.35/api/login',
-    APPOINTMENT_URL: 'https://77.162.207.35/appointments/',
+    API_URL: `http://77.162.207.35/api/`,
+    LOGIN_URL: 'http://77.162.207.35/api/login',
+    APPOINTMENT_URL: 'http://77.162.207.35/appointments/',
 };
